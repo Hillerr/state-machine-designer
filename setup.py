f63256623fbef94454a8fa43edfad521f6ced603
Hillerr <rfh@certi.org.br>
2020-07-18 22:47:00 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+import setuptools
 from setuptools import setup, find_packages
 
-setup(name="state_machine", packages=find_packages())
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="state_machine", 
+    packages=find_packages(),
+    version="0.0.0",
+    description="State machine designer and implementation package",
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+)
