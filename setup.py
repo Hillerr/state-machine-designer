e2315db9138bc4265c587aa6a29732225f1b13b1
Hiller <rafaelhiller23@gmail.com>
2020-07-26 17:52:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,16 @@ setup(
     name="state_machine", 
     packages=find_packages(),
     version="0.0.0",
+    author = 'Hillerr',                   # Type in your name
+    author_email = 'rafaelhiller23@gmail.com',
     description="State machine designer and implementation package",
     python_requires='>=3.6',
+    url = 'https://github.com/Hillerr/state-machine-designer'
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    download_url = 'https://github.com/Hillerr/state-machine-designer/archive/v0.0.0.tar.gz'
     install_requires=requirements,
 )
