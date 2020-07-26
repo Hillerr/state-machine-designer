2dae70da1683783c4e5a5c0ab196d4c8b3ef7120
Hiller <rafaelhiller23@gmail.com>
2020-07-25 23:47:21 -0300

    Update exceptions.py

--- state_machine/exceptions.py
+++ state_machine/exceptions.py
@@ -40,3 +40,25 @@ class AddStateError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
+
+
+class DecisionFunctionError(Exception):
+    """Exception raised for errors in the decision function 
+
+    Attributes:
+        message (str): The message that will be printed 
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UnknownStateError(Exception):
+    """Exception raised for unknown states in the states manager
+
+    Attributes:
+        message (str): The message that will be printed 
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
