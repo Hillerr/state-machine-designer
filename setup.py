925ae310011d816c42322c32ab7c5890776cbcc3
Hiller <rafaelhiller23@gmail.com>
2020-07-26 18:11:39 -0300

    Adding long description

--- setup.py
+++ setup.py
@@ -4,20 +4,26 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="state_machine", 
+    name="state-machine-designer", 
     packages=find_packages(),
     version="0.0.0",
     author = 'Hillerr',                   # Type in your name
     author_email = 'rafaelhiller23@gmail.com',
     description="State machine designer and implementation package",
+    long_description=long_description
+    long_description_content_type="text/markdown",
     python_requires='>=3.6',
-    url = 'https://github.com/Hillerr/state-machine-designer'
+    url = 'https://github.com/Hillerr/state-machine-designer',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    download_url = 'https://github.com/Hillerr/state-machine-designer/archive/v0.0.0.tar.gz'
+    download_url = 'https://github.com/Hillerr/state-machine-designer/archive/v0.0.0.tar.gz',
     install_requires=requirements,
+
 )
